db480524549fac2c5c49dd540206504e18d07568
Filip Vrba <filipvrba@pm.me>
2024-10-02 12:17:14 +0200

    update weather

--- src/js/elements/elm_weather_radar.js
+++ src/js/elements/elm_weather_radar.js
@@ -11,7 +11,8 @@ export default class ElmWeatherRadar extends HTMLElement {
     this._temperature = this.querySelector("#temperature");
     this._feelsLike = this.querySelector("#feelsLike");
     this._humidity = this.querySelector("#humidity");
-    this._wind = this.querySelector("#wind")
+    this._wind = this.querySelector("#wind");
+    this._time = this.querySelector("#time")
   };
 
   connectedCallback() {
@@ -31,21 +32,26 @@ export default class ElmWeatherRadar extends HTMLElement {
 
   initElm() {
     let template = `${`
-<div class='container my-5'>
-  <h2 class='text-center'>${this._words[0]}</h2>
-
-  <div class='row text-center mt-3'>
-    <div class='col-md-6 mb-3 mx-auto'>
-      <div class='card bg-light shadow'>
-        <div class='card-body'>
-          <h3 id='cityName' class='card-title'>Valencia</h3>
-          <img id='weatherIcon' class='' src='' alt='Weather' />
-          <p id='description' class='card-text'></p>
-          <p class='card-text'><strong>${this._words[1]}:</strong> <span id='temperature'></span></p>
-          <p class='card-text'><strong>${this._words[2]}:</strong> <span id='feelsLike'></span></p>
-          <p class='card-text'><strong>${this._words[3]}:</strong> <span id='humidity'></span></p>
-          <p class='card-text'><strong>${this._words[4]}:</strong> <span id='wind'></span></p>
-        </div>
+<div class='container'>
+  <div class='col-md-6 mb-3 mx-auto text-center'>
+    <div class=''>
+      <span style='color: #eb6e4b;' id='time'>Oct 2, 10:15am</span>
+      <h2 id='cityName'></h2>
+    </div>
+    <div>
+      <div class='d-flex mb-3 justify-content-center'>
+        <img id='weatherIcon' src='' width='50px' height='50px'>
+        <p class='h1 m-0 my-auto' id='temperature'>26°C</p>
+      </div>
+      <div class=''>
+        <p class='m-0'><strong id='description'>Feels like 25°C. Clear sky.</strong></p>
+        <ul class='d-flex flex-wrap list-unstyled justify-content-center'>
+          <li class='p-2' id='wind'>
+            <svg class='my-auto m-2' width='13px' data-v-47880d39='' viewBox='0 0 1000 1000' enable-background='new 0 0 1000 1000' xml:space='preserve' class='icon-wind-direction' style='transform: rotate(461deg);'><g data-v-47880d39='' fill='#48484a'><path data-v-47880d39='' d='M510.5,749.6c-14.9-9.9-38.1-9.9-53.1,1.7l-262,207.3c-14.9,11.6-21.6,6.6-14.9-11.6L474,48.1c5-16.6,14.9-18.2,21.6,0l325,898.7c6.6,16.6-1.7,23.2-14.9,11.6L510.5,749.6z'></path><path data-v-47880d39='' d='M817.2,990c-8.3,0-16.6-3.3-26.5-9.9L497.2,769.5c-5-3.3-18.2-3.3-23.2,0L210.3,976.7c-19.9,16.6-41.5,14.9-51.4,0c-6.6-9.9-8.3-21.6-3.3-38.1L449.1,39.8C459,13.3,477.3,10,483.9,10c6.6,0,24.9,3.3,34.8,29.8l325,898.7c5,14.9,5,28.2-1.7,38.1C837.1,985,827.2,990,817.2,990z M485.6,716.4c14.9,0,28.2,5,39.8,11.6l255.4,182.4L485.6,92.9l-267,814.2l223.9-177.4C454.1,721.4,469,716.4,485.6,716.4z'></path></g></svg>
+            4.9m/s W
+          </li>
+          <li class='p-2'>${this._words[3]}: <span id='humidity'>1010hPa</span></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -55,19 +61,36 @@ export default class ElmWeatherRadar extends HTMLElement {
   };
 
   subinitElm(weatherObj) {
-    this._cityName.innerText = weatherObj.name;
-    this._description.innerText = this._words[5][weatherObj.weather[0].description];
+    this._time.innerText = this.getDate(
+      weatherObj.dt,
+      weatherObj.timezone
+    );
+
+    this._cityName.innerText = `${weatherObj.name}, ${weatherObj.sys.country}`;
     let tempInCelsius = this.kelvinToCelsius(weatherObj.main.temp);
     this._temperature.innerText = `${tempInCelsius} °C`;
     let feelsInCelsius = this.kelvinToCelsius(weatherObj.main.feels_like);
-    this._feelsLike.innerText = `${feelsInCelsius} °C`;
+    this._description.innerText = `${this._words[2]} ${feelsInCelsius} °C. ${this._words[5][weatherObj.weather[0].description]}.`;
     this._humidity.innerText = `${weatherObj.main.humidity}%`;
-    this._wind.innerText = `${weatherObj.wind.speed} m/s`;
+    this._wind.innerHTML = `${`\n    <svg class='my-auto m-2' width='13px' data-v-47880d39='' viewBox='0 0 1000 1000' enable-background='new 0 0 1000 1000' xml:space='preserve' class='icon-wind-direction' style='transform: rotate(${weatherObj.wind.deg}deg);'><g data-v-47880d39='' fill='#48484a'><path data-v-47880d39='' d='M510.5,749.6c-14.9-9.9-38.1-9.9-53.1,1.7l-262,207.3c-14.9,11.6-21.6,6.6-14.9-11.6L474,48.1c5-16.6,14.9-18.2,21.6,0l325,898.7c6.6,16.6-1.7,23.2-14.9,11.6L510.5,749.6z'></path><path data-v-47880d39='' d='M817.2,990c-8.3,0-16.6-3.3-26.5-9.9L497.2,769.5c-5-3.3-18.2-3.3-23.2,0L210.3,976.7c-19.9,16.6-41.5,14.9-51.4,0c-6.6-9.9-8.3-21.6-3.3-38.1L449.1,39.8C459,13.3,477.3,10,483.9,10c6.6,0,24.9,3.3,34.8,29.8l325,898.7c5,14.9,5,28.2-1.7,38.1C837.1,985,827.2,990,817.2,990z M485.6,716.4c14.9,0,28.2,5,39.8,11.6l255.4,182.4L485.6,92.9l-267,814.2l223.9-177.4C454.1,721.4,469,716.4,485.6,716.4z'></path></g></svg>\n    ${weatherObj.wind.speed} m/s`}`;
     let iconCode = weatherObj.weather[0].icon;
     return this._weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
   };
 
   kelvinToCelsius(temperature) {
     return Math.round((temperature - 273.15).toFixed(2))
+  };
+
+  getDate(unixTimestamp, timezoneOffset) {
+    let options = {
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
+    };
+
+    let date = new Date((unixTimestamp + timezoneOffset) * 1_000);
+    return date.toLocaleString("en-US", options)
   }
 }
--- src/rb/elements/elm_weather_radar.js.rb
+++ src/rb/elements/elm_weather_radar.js.rb
@@ -16,6 +16,7 @@ export default class ElmWeatherRadar < HTMLElement
     @feels_like   = self.query_selector('#feelsLike')
     @humidity     = self.query_selector('#humidity')
     @wind         = self.query_selector('#wind')
+    @time         = self.query_selector('#time')
   end
 
   def connected_callback()
@@ -30,21 +31,26 @@ export default class ElmWeatherRadar < HTMLElement
 
   def init_elm()
     template = """
-<div class='container my-5'>
-  <h2 class='text-center'>#{@words[0]}</h2>
-
-  <div class='row text-center mt-3'>
-    <div class='col-md-6 mb-3 mx-auto'>
-      <div class='card bg-light shadow'>
-        <div class='card-body'>
-          <h3 id='cityName' class='card-title'>Valencia</h3>
-          <img id='weatherIcon' class='' src='' alt='Weather' />
-          <p id='description' class='card-text'></p>
-          <p class='card-text'><strong>#{@words[1]}:</strong> <span id='temperature'></span></p>
-          <p class='card-text'><strong>#{@words[2]}:</strong> <span id='feelsLike'></span></p>
-          <p class='card-text'><strong>#{@words[3]}:</strong> <span id='humidity'></span></p>
-          <p class='card-text'><strong>#{@words[4]}:</strong> <span id='wind'></span></p>
-        </div>
+<div class='container'>
+  <div class='col-md-6 mb-3 mx-auto text-center'>
+    <div class=''>
+      <span style='color: #eb6e4b;' id='time'>Oct 2, 10:15am</span>
+      <h2 id='cityName'></h2>
+    </div>
+    <div>
+      <div class='d-flex mb-3 justify-content-center'>
+        <img id='weatherIcon' src='' width='50px' height='50px'>
+        <p class='h1 m-0 my-auto' id='temperature'>26°C</p>
+      </div>
+      <div class=''>
+        <p class='m-0'><strong id='description'>Feels like 25°C. Clear sky.</strong></p>
+        <ul class='d-flex flex-wrap list-unstyled justify-content-center'>
+          <li class='p-2' id='wind'>
+            <svg class='my-auto m-2' width='13px' data-v-47880d39='' viewBox='0 0 1000 1000' enable-background='new 0 0 1000 1000' xml:space='preserve' class='icon-wind-direction' style='transform: rotate(461deg);'><g data-v-47880d39='' fill='#48484a'><path data-v-47880d39='' d='M510.5,749.6c-14.9-9.9-38.1-9.9-53.1,1.7l-262,207.3c-14.9,11.6-21.6,6.6-14.9-11.6L474,48.1c5-16.6,14.9-18.2,21.6,0l325,898.7c6.6,16.6-1.7,23.2-14.9,11.6L510.5,749.6z'></path><path data-v-47880d39='' d='M817.2,990c-8.3,0-16.6-3.3-26.5-9.9L497.2,769.5c-5-3.3-18.2-3.3-23.2,0L210.3,976.7c-19.9,16.6-41.5,14.9-51.4,0c-6.6-9.9-8.3-21.6-3.3-38.1L449.1,39.8C459,13.3,477.3,10,483.9,10c6.6,0,24.9,3.3,34.8,29.8l325,898.7c5,14.9,5,28.2-1.7,38.1C837.1,985,827.2,990,817.2,990z M485.6,716.4c14.9,0,28.2,5,39.8,11.6l255.4,182.4L485.6,92.9l-267,814.2l223.9-177.4C454.1,721.4,469,716.4,485.6,716.4z'></path></g></svg>
+            4.9m/s W
+          </li>
+          <li class='p-2'>#{@words[3]}: <span id='humidity'>1010hPa</span></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -55,16 +61,19 @@ export default class ElmWeatherRadar < HTMLElement
   end
 
   def subinit_elm(weather_obj)
-    @city_name.inner_text = weather_obj.name
-    @description.inner_text = @words[5][weather_obj.weather[0].description]
+    @time.inner_text = get_date(weather_obj.dt, weather_obj.timezone)
+    @city_name.inner_text = "#{weather_obj.name}, #{weather_obj.sys.country}"
 
     temp_in_celsius = kelvin_to_celsius(weather_obj.main.temp)
     @temperature.inner_text = "#{temp_in_celsius} °C"
     feels_in_celsius = kelvin_to_celsius(weather_obj.main['feels_like'])
-    @feels_like.inner_text = "#{feels_in_celsius} °C"
+
+    @description.inner_text = "#{@words[2]} #{feels_in_celsius} °C. #{@words[5][weather_obj.weather[0].description]}."
 
     @humidity.inner_text = "#{weather_obj.main.humidity}%"
-    @wind.inner_text = "#{weather_obj.wind.speed} m/s"
+    @wind.innerHTML = """
+    <svg class='my-auto m-2' width='13px' data-v-47880d39='' viewBox='0 0 1000 1000' enable-background='new 0 0 1000 1000' xml:space='preserve' class='icon-wind-direction' style='transform: rotate(#{weather_obj.wind.deg}deg);'><g data-v-47880d39='' fill='#48484a'><path data-v-47880d39='' d='M510.5,749.6c-14.9-9.9-38.1-9.9-53.1,1.7l-262,207.3c-14.9,11.6-21.6,6.6-14.9-11.6L474,48.1c5-16.6,14.9-18.2,21.6,0l325,898.7c6.6,16.6-1.7,23.2-14.9,11.6L510.5,749.6z'></path><path data-v-47880d39='' d='M817.2,990c-8.3,0-16.6-3.3-26.5-9.9L497.2,769.5c-5-3.3-18.2-3.3-23.2,0L210.3,976.7c-19.9,16.6-41.5,14.9-51.4,0c-6.6-9.9-8.3-21.6-3.3-38.1L449.1,39.8C459,13.3,477.3,10,483.9,10c6.6,0,24.9,3.3,34.8,29.8l325,898.7c5,14.9,5,28.2-1.7,38.1C837.1,985,827.2,990,817.2,990z M485.6,716.4c14.9,0,28.2,5,39.8,11.6l255.4,182.4L485.6,92.9l-267,814.2l223.9-177.4C454.1,721.4,469,716.4,485.6,716.4z'></path></g></svg>
+    #{weather_obj.wind.speed} m/s"""
 
     icon_code = weather_obj.weather[0].icon
     @weather_icon.src = "https://openweathermap.org/img/wn/#{icon_code}@2x.png"
@@ -72,5 +81,18 @@ export default class ElmWeatherRadar < HTMLElement
 
   def kelvin_to_celsius(temperature)
     return Math.round( (temperature - 273.15).to_fixed(2) )
+  end
+
+  def get_date(unix_timestamp, timezone_offset)
+    options = {
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    }
+
+    date = Date.new (unixTimestamp + timezoneOffset) * 1000
+    return date.to_locale_string('en-US', options)
   end
 end
